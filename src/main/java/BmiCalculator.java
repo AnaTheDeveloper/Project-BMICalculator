da092456ad551b1d2b81903c449aef202f9f98af
MinniesTea <edwards.ana81@gmail.com>
2021-05-24 12:07:32 +0100

    V2

--- src/main/java/BmiCalculator.java
+++ src/main/java/BmiCalculator.java
@@ -1,20 +1,31 @@
 public class BmiCalculator {
 
+  String name = "";
+  double height = 0.0;          //In feet and inches | Cm
+  double weight = 0.0;          //In stones and pounds | KG
 
-  //instance variables - height weight name
-  //constructor 2 parameters weight height name
-  //getters and setters in order to change height and weight
+  public BmiCalculator(String name, double height, double weight){
 
-  //method that uses formula (BMI= weight/heightsquared)
+    this.name = name;
+    this.height = height;
+    this.weight = weight;
 
-//  if bmi <= 18.5 return "Underweight"
-//      if bmi <= 25.0 return "Normal"
-//      if bmi <= 30.0 return "Overweight"
-//      if bmi > 30 return "Obese"
+  }
 
+  public String bmiFormula(){
 
-  //return a string with appropriate response
-  //method does not accept any parameters it uses instance variables from constructor
+    double bmi = weight / height * height;
 
+    if (bmi <= 18.5) {
+      return name + ", You are underweight.";
+    }else if (bmi <= 25.0) {
+      return name + ", You are normal weight.";
+    }  else if (bmi <= 30.0){
+      return name + ", You are overweight.";
+    }  else if (bmi > 30){
+      return name + ", You are obese.";
+    } else {
+      return name + ", Thank you for using this BMI calculator.";
+    }
 
-}
+}}
--- src/main/java/BmiCalculatorMain.java
+++ src/main/java/BmiCalculatorMain.java
@@ -3,5 +3,8 @@ public class BmiCalculatorMain {
 
   public static void main(String[] args) {
 
+    BmiCalculator bmiCalculator = new BmiCalculator("Ana", 160, 50);
+
+    System.out.println(bmiCalculator.bmiFormula());
   }
 }
