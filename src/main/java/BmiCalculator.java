5586e613ff01d5467fe451c00c7517526150b9cf
MinniesTea <edwards.ana81@gmail.com>
2021-05-24 11:22:17 +0100

    BMI calculator template layout V1

---
+++ src/main/java/BmiCalculator.java
@@ -0,0 +1,20 @@
+public class BmiCalculator {
+
+
+  //instance variables - height weight name
+  //constructor 2 parameters weight height name
+  //getters and setters in order to change height and weight
+
+  //method that uses formula (BMI= weight/heightsquared)
+
+//  if bmi <= 18.5 return "Underweight"
+//      if bmi <= 25.0 return "Normal"
+//      if bmi <= 30.0 return "Overweight"
+//      if bmi > 30 return "Obese"
+
+
+  //return a string with appropriate response
+  //method does not accept any parameters it uses instance variables from constructor
+
+
+}
---
+++ src/main/java/BmiCalculatorMain.java
@@ -0,0 +1,7 @@
+public class BmiCalculatorMain {
+
+
+  public static void main(String[] args) {
+
+  }
+}
